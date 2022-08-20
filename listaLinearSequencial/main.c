44ede42f4edea7cefdbdbb695086725f63fbfb9c
Gustavo Ferreira <70302908+Gussstavof@users.noreply.github.com>
2022-08-20 16:09:21 -0300

    lista linear sequencial

---
+++ listaLinearSequencial/main.c
@@ -0,0 +1,95 @@
+//
+// Created by GustavoF on 8/20/2022.
+//
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <malloc.h>
+
+typedef int TYPEKEY;
+
+typedef struct{
+    TYPEKEY key;
+}Registry;
+
+typedef struct {
+    Registry A[10+1];
+    int elementNumber;
+}List;
+
+void initList(List* list){
+    list->elementNumber = 0;
+}
+
+int length(List* list){
+    return list->elementNumber;
+}
+
+void showList(List* list){
+    int i;
+    for(i=0; i < list->elementNumber; i++){
+        printf("%i ", list->A[i].key);
+    }
+    printf("\n");
+}
+
+int searchElement(List* list, TYPEKEY key){
+    int i = 0;
+    list->A[list->elementNumber].key = key;
+    while (list->A[i].key != key) i++;
+    if(i == list->elementNumber){
+        return -1;
+    }
+
+    return i;
+}
+
+bool addElement(List* list, Registry registry, int i){
+    int x;
+    if((list->elementNumber == 10) || (i < 0) || (i > list->elementNumber)){
+        return false;
+    }
+    for (x = list->elementNumber; x > i; --x) {
+        list->A[x] = list->A[x-1];
+    }
+    list->A[i] = registry;
+    list->elementNumber++;
+    return true;
+}
+
+bool deleteElement(TYPEKEY key, List* list){
+    int position, x;
+    position = searchElement(list, key);
+    if (position == -1){
+        return false;
+    }
+    for (x = position; x < list->elementNumber-1; x++) {
+        list->A[x] = list->A[x+1];
+    }
+    list->elementNumber--;
+    return true;
+}
+
+void resetList(List* list){
+    list->elementNumber = 0;
+}
+
+int main(){
+    List* list = (List*) malloc(sizeof (List));
+    Registry registry1;
+    registry1.key = 1;
+    initList(list);
+    addElement(list, registry1, 0);
+    showList(list);
+    registry1.key = 5;
+    addElement(list, registry1, 0);
+    showList(list);
+    registry1.key = 7;
+    addElement(list, registry1, 1);
+    showList(list);
+    printf("length: %i \n", length(list));
+    deleteElement(5,list);
+    showList(list);
+    printf("length: %i \n", length(list));
+    return 0;
+}
